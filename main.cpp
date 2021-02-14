271a763a85dc208d7ae99ce889331eb273cafbf4
Wutipong Wongsakuldej <wutipong@trueaxiongames.com>
2021-02-14 18:06:09 +0700

    move some GL-specific constant up to the begining of the file.

--- main.cpp
+++ main.cpp
@@ -5,19 +5,23 @@
 #include "imgui_impl/imgui_impl_opengl3.h"
 #include "imgui_impl/imgui_impl_sdl.h"
 
-constexpr int WIDTH = 800;
-constexpr int HEIGHT = 600;
+constexpr int WindowWidth = 800;
+constexpr int WindowHeight = 600;
+
+constexpr char GlslVersion[] = "#version 450";
+constexpr int GlMajorVersion = 4;
+constexpr int GlMinorVersion = 5;
 
 int main(int argc, char **argv) {
   SDL_Init(SDL_INIT_EVERYTHING);
 
-  const char *glsl_version = "#version 450";
   SDL_GL_SetAttribute(
       SDL_GL_CONTEXT_FLAGS,
-      SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+      SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac4
+
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, GlMajorVersion);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, GlMinorVersion);
 
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
@@ -25,10 +29,10 @@ int main(int argc, char **argv) {
 
   SDL_Window *window = SDL_CreateWindow(
       "sdl2-gl-project-template", SDL_WINDOWPOS_UNDEFINED,
-      SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT,
+      SDL_WINDOWPOS_UNDEFINED, WindowWidth, WindowHeight,
       SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_OPENGL);
 
-  SDL_SetWindowMinimumSize(window, WIDTH, HEIGHT);
+  SDL_SetWindowMinimumSize(window, WindowWidth, WindowHeight);
 
   SDL_GLContext glCtx = SDL_GL_CreateContext(window);
   SDL_GL_MakeCurrent(window, glCtx);
@@ -41,7 +45,7 @@ int main(int argc, char **argv) {
   ImGuiIO &io = ImGui::GetIO();
 
   ImGui_ImplSDL2_InitForOpenGL(window, glCtx);
-  ImGui_ImplOpenGL3_Init(glsl_version);
+  ImGui_ImplOpenGL3_Init(GlslVersion);
 
   while (true) {
     SDL_Event event;
@@ -55,7 +59,11 @@ int main(int argc, char **argv) {
     ImGui_ImplSDL2_NewFrame(window);
     ImGui::NewFrame();
 
-    // Draw UI
+    // Begin Draw UI
+    ImGui::Begin("Hello");
+    ImGui::LabelText("Hello", "Hello World");
+    ImGui::End();
+    // End Draw UI
 
     ImGui::EndFrame();
     ImGui::Render();
@@ -67,6 +75,8 @@ int main(int argc, char **argv) {
 
     glViewport(0, 0, actualHeight, actualHeight);
     glDisable(GL_SCISSOR_TEST);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
