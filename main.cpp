ab15fdae02f042960b1d87d1b172f88ec8cda00c
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2023-04-22 18:15:30 +0700

    update vcpkg to the latest version.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <glm/glm.hpp>
 #include <imgui.h>
 #include <imgui_impl_opengl3.h>
-#include <imgui_impl_sdl.h>
+#include <imgui_impl_sdl2.h>
 #include <spdlog/spdlog.h>
 
 #include "shader.hpp"
